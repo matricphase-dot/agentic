f169bc1122575a89a7da1a775c10a4e211006f16
GitHub Action <action@github.com>
2025-12-29 17:17:28 +0530

    Emergency restore: working dashboard

--- web/app.py
+++ web/app.py
@@ -1673,22 +1673,3 @@ if __name__ == "__main__":
     use_reloader=False,
 )
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///app.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///app.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-from waitlist import waitlist_bp
-app.register_blueprint(waitlist_bp, name='waitlist_page')
-# Waitlist functionality
-from waitlist import waitlist_bp
-app.register_blueprint(waitlist_bp, name='waitlist_new')
